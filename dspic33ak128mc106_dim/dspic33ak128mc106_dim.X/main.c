a3240955b2b069749293735a3d61b427d935d315
Yash Bhalchandra <Yash.Bhalchandra@microchip.com>
2024-02-22 11:43:06 -0700

    Basic I/O Demo

--- dspic33ak128mc106_dim/dspic33ak128mc106_dim.X/main.c
+++ dspic33ak128mc106_dim/dspic33ak128mc106_dim.X/main.c
@@ -20,12 +20,71 @@
 */
 
 #include "mcc_generated_files/system/system.h"
+#include "bsp/led_blue.h"
+#include "bsp/led_red.h"
+#include "bsp/led_green.h"
+#include "bsp/led7.h"
+#include "bsp/led6.h"
+#include "bsp/led5.h"
+#include "bsp/led4.h"
+#include "bsp/led3.h"
+#include "bsp/led2.h"
+#include "bsp/led1.h"
+#include "bsp/led0.h"
+#include "bsp/s1.h"
+#include "bsp/s2.h"
+#include "bsp/s3.h"
+
+void initializeAllLEDs(void){
+    ledRed.initialize();
+    ledGreen.initialize();
+    ledBlue.initialize();
+    led0.initialize();
+    led1.initialize();
+    led2.initialize();
+    led3.initialize();
+    led4.initialize();
+    led5.initialize();
+    led6.initialize();
+    led7.initialize();
+}
+
+void initializeAllButtons(void){
+    s1.initialize();
+    s2.initialize();
+    s3.initialize();
+}
+
+void turnOffAllLEDs(void){
+    led0.off();
+    led1.off();
+    led2.off();
+    led3.off();
+    led4.off();
+    led5.off();
+    led6.off();
+    led7.off();
+}
 
 int main(void)
 {       
     SYSTEM_Initialize();
+    initializeAllLEDs();
+    initializeAllButtons();
     
     while(1)
     {  
+        if (s1.isPressed()){
+            led7.on();
+        }
+        else if(s2.isPressed()){
+            led6.on();
+        }
+        else if(s3.isPressed()){
+            led5.on();
+        }
+        else{
+            turnOffAllLEDs();
+        }
     }    
 }
