00395c0b08fb3f98c12aaaf79f6f7e0caa881003
Yash Bhalchandra <Yash.Bhalchandra@microchip.com>
2024-03-01 12:38:10 -0700

    add demo

--- dspic33ak512mps512_dim/dspic33ak512mps512_dim.X/main.c
+++ dspic33ak512mps512_dim/dspic33ak512mps512_dim.X/main.c
@@ -20,13 +20,177 @@
 */
 
 #include "mcc_generated_files/system/system.h"
+#include "bsp/led7.h"
+#include "bsp/led6.h"
+#include "bsp/led5.h"
+#include "bsp/led_red.h"
+#include "bsp/led_blue.h"
+#include "bsp/led_green.h"
+#include "bsp/led_color.h"
+#include "bsp/led_rgb.h"
+#include "bsp/s1.h"
+#include "bsp/s2.h"
+#include "bsp/s3.h"
+#include "mcc_generated_files/uart/uart1.h"
+#include "bsp/pot.h"
+#include "bsp/task.h"
+#include <stdio.h>
+
+static bool potentiometerPrintRequired = false;
+
+static void initializeAllLEDs(void)
+{
+    ledRGB.initialize();
+    led5.initialize();
+    led6.initialize();
+    led7.initialize();
+}
+
+static void initializeAllButtons(void)
+{
+    s1.initialize();
+    s2.initialize();
+    s3.initialize();
+}
+
+static void setRGBIntensity(uint16_t potentiometerReading)
+{
+    ledRed.setIntensity(potentiometerReading);
+    ledGreen.setIntensity(potentiometerReading);
+    ledBlue.setIntensity(potentiometerReading); 
+}
+
+static void clearTerminalScreen(void)
+{
+    printf("\033[2J"); 
+}
+
+static void moveCursor(int row)
+{
+    printf("\033[%d;0f", row);
+}
+
+static void hideCursor()
+{
+    printf("\033[?25l");
+}
+
+static void printMenu(void)
+{
+    clearTerminalScreen();
+    hideCursor();
+    moveCursor(1);
+    printf("------------------------------------ \r\n");
+    printf("dsPIC33AK512MPS512 Out of Box Demo\r\n");
+    printf("------------------------------------ \r\n");   
+    printf("Hold Switch S1 - LED7 turns on \r\n");
+    printf("Hold Switch S2 - LED6 turns on \r\n");
+    printf("Hold Switch S3 - LED5 turns on \r\n");
+    printf("Press 'r', 'g', 'b' will toggle red, green, and blue states of the RGB LED "
+            "respectively \r\n");
+    printf("Turning the potentiometer will adjust the intensity of the RGB LED \r\n");
+
+}
+
+static void printPotentiometer(void)
+{
+    potentiometerPrintRequired = true;
+}
+ 
+static void checkUartApp(void)
+{
+    if(UART1_IsRxReady())
+    {
+        uint8_t dataRx = UART1_Read();
+        if(dataRx)
+        {
+            switch(dataRx)
+            {
+                case 'r': 
+                case 'R': 
+                    ledRed.toggle();
+                    break;
+                case 'g': 
+                case 'G': 
+                    ledGreen.toggle();
+                    break;   
+                case 'b': 
+                case 'B': 
+                    ledBlue.toggle();
+                    break;
+                default: 
+                    break;
+            }
+        }   
+
+        while(!UART1_IsTxReady()) 
+        {     
+        }
+
+        UART1_Write(dataRx);       
+    }
+}
+ 
+static void checkButtonS1(void)
+{
+    if(s1.isPressed()) 
+    {
+        led7.on();
+    } 
+    else 
+    {
+        led7.off();
+    }
+}
+ 
+static void checkButtonS2(void)
+{
+    if(s2.isPressed()) 
+    {
+       led6.on();
+    } 
+    else 
+    {
+       led6.off();
+    }    
+}
+ 
+static void checkButtonS3(void)
+{
+    if(s3.isPressed()) 
+    {
+        led5.on();
+    } 
+    else {
+        led5.off();
+    } 
+}
 
 int main(void)
 {       
-    SYSTEM_Initialize();   
-    
-    while(1)
+    SYSTEM_Initialize();
+    initializeAllLEDs();
+    initializeAllButtons();
+    TASK_Initialize();
+    pot.initialize();
+    ledRGB.on();
+    printMenu();
+    TASK_Request(printPotentiometer, 200);
+
+    while (1) 
     {
+        uint16_t potentiometerReading = pot.read();
+        setRGBIntensity(potentiometerReading);
+        if(potentiometerPrintRequired)
+        {
+            potentiometerPrintRequired = false;
+            moveCursor(10);
+            printf("Potentiometer: 0x%04X\r\n", pot.read());
+        }
         
+        checkUartApp();
+        checkButtonS1();
+        checkButtonS2();
+        checkButtonS3();
     }
 }
